06b14e1fc596c6c9f18d57f687f9e66afe0a3673
yasink@saltire.com <yasink@saltire.com>
2024-07-25 15:42:59 -0700

    Create script to refresh test list

---
+++ refresh_test_list.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+> "//192.168.50.73/ForReview/Casio/fx-CG100 emulator/automated-testing/all-tests.txt"
+cp "//192.168.50.73/ForReview/Casio/fx-CG100 emulator/automated-testing/all-tests-backup.txt" "//192.168.50.73/ForReview/Casio/fx-CG100 emulator/automated-testing/all-tests.txt"
+exit 0
